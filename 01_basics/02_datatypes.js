c20bac634dc9ff2fba934d896fbef4c5e8f3dd4a
Aashish70 <ashishsingh70002@gmail.com>
2024-03-08 01:04:07 +0530

    Add data types examples

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all JS code as newer version
+
+// alert("Hello, World!"); // we are using node.js, so this will not work
+
+console.log("ashish")
+
+let name = "ashish";
+let age = 20;
+let isLoggedIn = true;
+let State;
+
+// Number => 2 to the power 53
+// BigInt 
+// String = ""
+// boolean = true/false
+// undefined
+// Symbol => unique
+//object
+
+console.log(typeof undefined)
+console.log(typeof null)
+
+
