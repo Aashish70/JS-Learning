515326e78a9b3b8b304b13a572cef58ef29a261b
Aashish70 <ashishsingh70002@gmail.com>
2024-03-08 21:03:13 +0530

    Add array manipulation examples

---
+++ 02_ basics/01_arrays.js
@@ -0,0 +1,48 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const myHeroes = ["shakatiman", "naagraj", "doga", "superman", "batman", "spiderman", "ironman", "hulk", "thor", "captain america"]
+
+// console.log(typeof myArr)
+
+const myArr2 = new Array(23, 45, 56, 78, 89, 90)
+
+// console.log(typeof myArr2)
+
+//Array Methods
+
+// myArr.pop(9)
+// myArr.push(10)
+// myArr.push(11)
+
+console.log(myArr)
+
+
+// myArr.unshift()
+// myArr.unshift(11)
+// myArr.shift()
+// myArr.shift()
+
+// console.log(myArr.includes(7))
+// console.log(myArr.indexOf(1))
+
+const newArr = myArr.join()
+
+// console.log(newArr)
+// console.log(myArr)
+// console.log(typeof newArr)
+
+//slice, splice
+
+// console.log("A", myArr)
+
+const myn1 = myArr.slice(1, 3)
+ console.log(myn1)
+
+// console.log("B", myArr)
+
+const myn2 = myArr.splice(1, 3)
+console.log(myn2)
+
+
+
