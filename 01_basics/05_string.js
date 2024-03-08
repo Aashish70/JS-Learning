d724922954f6ca4e1631789609d6a008f9b7d9cc
Aashish70 <ashishsingh70002@gmail.com>
2024-03-08 17:37:18 +0530

    Add string manipulation examples

---
+++ 01_basics/05_string.js
@@ -0,0 +1,36 @@
+const name = "ashish";
+const repoCount = 20
+
+// console.log(name + repoCount + " Values")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String("ashish-com") 
+
+// console.log(gameName[0])
+// console.log(gameName.charAt(0))
+
+
+// console.log(gameName.__proto__)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.length)
+
+// console.log(gameName.indexOf("c"))
+
+const newString = gameName.substring(0,4)
+// console.log(newString)
+
+const anotherString = gameName.slice(0,4)
+// console.log(anotherString)
+
+const newStringOne = "    ashish    "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+const url = "https://ashish.com/ashish%300kumar"
+
+// console.log(url.replace("%300", "-"))
+
+// console.log(url.includes("funny")) 
+
+console.log(gameName.split("-")) 
